2f42270ca7529479a26860600d089306147df879
Ali Awan <ali.raza@itskillxpress.com>
2024-07-28 00:17:37 +0500

    added insta nd twitter channel with auth

--- src/routes/twitter.js
+++ src/routes/twitter.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-const twitterController = require("../controllers/twitter");
+const twitterController = require("../controllers/mainTwitter");
+const { auth } = require('../middlewares/auth');
 
 // POST /twitter/post-text-tweet - send text tweet to twitter account
-router.post("/post-text-tweet", twitterController.postTextTweetToTwitter);
+router.post("/post-text-tweet",auth, twitterController.postTextTweetToTwitter);
 
 // POST /twitter/post-image-tweet - send image tweet to twitter account
-router.post("/post-image-tweet", twitterController.postImageTweetToTwitter);
+router.post("/post-image-tweet",auth, twitterController.postImageTweetToTwitter);
 
 // POST /twitter/post-carousel-tweet - send carousel tweet to twitter account
-router.post("/post-carousel-tweet", twitterController.postCarouselTweetToTwitter);
+router.post("/post-carousel-tweet",auth, twitterController.postCarouselTweetToTwitter);
                                 
 // POST /twitter/post-video-tweet - send video tweet to twitter account
-router.post("/post-video-tweet", twitterController.postVideoTweetToTwitter);
+router.post("/post-video-tweet",auth, twitterController.postVideoTweetToTwitter);
 
 module.exports = router;
